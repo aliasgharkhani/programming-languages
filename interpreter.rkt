988ec41a2cddd5b0c88f13995ed798ac514a9f78
ali asghar khani <khani.ali1376@gmail.com>
2020-06-06 20:19:58 +0430

    first parts of interpreter

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,30 +1,145 @@
-(define if-com
-  (lambda () ()))
+;predicators
+(define program?
+  (lambda (command)
+    (cond
+      ((null? (cdr command)) (unitcom? (car command)))
+      (else (and (unitcom? (car command)) (program? (cdr command)))))
+    ))
+
+(define unitcom?
+  (lambda (unitcom) (or (while-com? unitcom) (if-com? unitcom) (assign? unitcom) (return? unitcom))))
 
 (define while-com?
-  (lambda (unit-com) (eqv? (car(unit-com)) 'while)))
+  (lambda (unit-com) (eqv? (car (unit-com)) 'while)))
 
 (define if-com?
-  (lambda (unit-com) (eqv? (car(unit-com)) 'if)))
+  (lambda (unit-com) (eqv? (car (unit-com)) 'if)))
 
 (define assign?
-  (lambda (unit-com) (eqv? (car(unit-com)) 'assign)))
-
-(define assign?
-  (lambda (unit-com) (eqv? (car(unit-com)) 'assign)))
+  (lambda (unit-com) (eqv? (car (unit-com)) 'assign)))
 
 (define return?
-  (lambda (unit-com) (eqv? (car(unit-com)) 'return)))
+  (lambda (unit-com) (eqv? (car unit-com) 'return)))
 
 (define more?
-  (lambda (exp) (eqv? (car(exp)) 'more?)))
+  (lambda (exp) (eqv? (car exp) 'more?)))
 
 (define less?
-  (lambda (exp) (eqv? (car(exp)) 'less?)))
+  (lambda (exp) (eqv? (car exp) 'less?)))
 
 (define equal?
-  (lambda (exp) (eqv? (car(exp)) 'equal?)))
+  (lambda (exp) (eqv? (car exp) 'equal?)))
 
 (define nequal?
-  (lambda (exp) (eqv? (car(exp)) 'nequal?)))
+  (lambda (exp) (eqv? (car exp) 'nequal?)))
+
+(define sub?
+  (lambda (exp) (eqv? (car exp) 'sub?)))
+
+(define add?
+  (lambda (exp) (eqv? (car exp) 'add?)))
+
+(define mult?
+  (lambda (exp) (eqv? (car exp) 'mult?)))
+
+(define div?
+  (lambda (exp) (eqv? (car exp) 'div?)))
+
+(define symmetric?
+  (lambda (exp) (eqv? (car exp) '-)))
+
+(define my-null?
+  (lambda (exp) (eqv? exp 'null)))
+
+(define var?
+  (lambda (exp) (eqv? (car exp) 'var)))
+
+(define var-listmem?
+  (lambda (cexp) (eqv? (car cexp) 'list-ref?)))
+
+;extractors
+
+(define while-com->exp
+  (lambda (unit-com) (caadr unit-com)))
+
+(define while-com->command
+  (lambda (unit-com) (caaddr unit-com)))
+
+(define if-com->exp
+  (lambda (unit-com) (caadr unit-com)))
+
+(define if-com->com1
+  (lambda (unit-com) (caaddr unit-com)))
+
+(define if-com->com2
+  (lambda (unit-com) (caadddr unit-com)))
+
+(define assign->var
+  (lambda (unit-com) (cadr unit-com)))
+
+(define assign->exp
+  (lambda (unit-com) (caddr unit-com)))
+
+(define return->exp
+  (lambda (unit-com) (cadr unit-com)))
+
+(define exp->aexp1
+  (lambda (exp) (cadr exp)))
+
+(define exp->aexp2
+  (lambda (exp) (caddr exp)))
+
+(define aexp->bexp
+  (lambda (aexp) (cadr aexp)))
+
+(define aexp->aexp
+  (lambda (aexp) (caddr aexp)))
+
+(define bexp->cexp
+  (lambda (bexp) (cadr bexp)))
+
+(define bexp->bexp
+  (lambda (bexp) (caddr bexp)))
+
+(define cexp->cexp
+  (lambda (cexp) (cadr cexp)))
+
+(define cexp->exp
+  (lambda (cexp) (car cexp)))
+
+(define cexp->var
+  (lambda (cexp) (cadr cexp)))
+
+(define cexp->listmem
+  (lambda (cexp) (caddr cexp)))
+
+
+
+
+
+
+;interpreter
+
+(define run
+  (lambda (string)
+    (value-of-program (lexer-parser string))))
+
+(define value-of-program
+  (lambda (pgm)
+    (cond
+      ((program? pgm) (value-of pgm (init-env))))
+    ))
+
+(define value-of
+  (lambda (unitcom env)
+    (cond
+      ((while-com? unitcom) (??????????))
+      ((if-com? unitcom) (let ((exp (value-of (cadr unitcom) env)))
+                           (if (exp)
+                               (value-of (caddr unitcom) env)
+                               (value-of (cadddr unitcom) env))))
+      ((assign? unitcom) (extend-env (cadr unitcom) (value-of (caddr unitcom) env)))
+      ((return? unitcom) (value-of (cadr unitcom)))
+      
+      )))
 
--- parser.rkt
+++ parser.rkt
@@ -20,8 +20,8 @@
             ("false" (token-false))
             ("string" (token-string (lexeme)))
             ((:or (:+ (char-range #\0 #\9)) (:: (:+ (char-range #\0 #\9)) #\. (:+ (char-range #\0 #\9)))) (token-posnumber (string->number lexeme)))
-            ((:+ (char-range #\a #\z)) (token-var lexeme))
-            ("+" (token-plus))
+            ((:+ (char-range #\a #\z)) (token-var (string->symbol lexeme)))
+            ("+" (token-pos))
             ("==" (token-eq))
             ("=" (token-eq-assign))
             ("-" (token-neg))
@@ -42,7 +42,7 @@
             ((eof) (token-EOF))))
 
 (define-tokens a (var string posnumber))
-(define-empty-tokens b (EOF plus neg eq-assign eq mult div less more neq lpar rpar lbrack rbrack if semicolon camma while do end then else endif returnt null true false))
+(define-empty-tokens b (EOF pos neg eq-assign eq mult div less more neq lpar rpar lbrack rbrack if semicolon camma while do end then else endif returnt null true false))
 
 
 (define simple-math-parser
@@ -52,23 +52,23 @@
             (error void)
             (tokens b a)
             (grammar
-           (command ((unitcom) $1) ((command semicolon unitcom) (list $1  $3)))
+           (command ((unitcom) (list $1)) ((command semicolon unitcom) (append $1  (list $3))))
            (unitcom ((whilecom) $1) ((ifcom) $1) ((assign) $1) ((return) $1))
            (whilecom ((while exp do command end) (list 'while ($2) ($4))))
            (ifcom ((if exp then command else command endif) (list 'if (list $2) (list $4) (list $6))))
            (assign ((var eq-assign exp) (list 'assign $1 $3)))
            (return ((returnt exp) (list 'return $2)))
            (exp ((aexp) $1) ((aexp more aexp) (list 'more? $1 $3)) ((aexp less aexp) (list 'less? $1 $3)) ((aexp eq aexp) (list 'equal? $1 $3)) ((aexp neq aexp) (list 'nequal? $1 $3)))
-           (aexp ((bexp) $1) ((bexp neg aexp) (list '- $1 $3)) ((bexp plus aexp) (list '+ $1 $3)))
-           (bexp ((cexp) $1) ((cexp mult bexp) (list '* $1 $3)) ((cexp div bexp) (list '/ $1 $3)))
-           (cexp ((neg cexp) (list '- 0 $2)) ((lpar exp rpar) (list $2)) ((posnumber) $1) ((null) 'null) ((var) $1) ((true) '#t) ((false) '#f) ((string) $1) ((list) $1) ((var listmem) (list 'list-ref $1 $2)))
-           (list ((lbrack listvalues rbrack) (list $2)) ((lbrack rbrack) (list)))
-           (listvalues ((exp) $1) ((exp camma listvalues) (list $1 $3)))
+           (aexp ((bexp) $1) ((bexp neg aexp) (list 'sub $1 $3)) ((bexp pos aexp) (list 'add $1 $3)))
+           (bexp ((cexp) $1) ((cexp mult bexp) (list 'mult $1 $3)) ((cexp div bexp) (list 'div $1 $3)))
+           (cexp ((neg cexp) (list '- $2)) ((lpar exp rpar) (list $2)) ((posnumber) $1) ((null) 'null) ((var) ('var $1)) ((true) '#t) ((false) '#f) ((string) $1) ((list) $1) ((var listmem) (list 'list-ref $1 $2)))
+           (list ((lbrack listvalues rbrack) $2) ((lbrack rbrack) (list)))
+           (listvalues ((exp) (list $1)) ((exp camma listvalues) (append (list $1) $3)))
            (listmem ((lbrack exp rbrack) (list $2)) ((lbrack exp rbrack listmem) (list $2 $4)))
              )))
 
-;test
+;
 (define lex-this (lambda (lexer input) (lambda () (lexer input))))
-(define my-lexer (lex-this simple-math-lexer (open-input-string "return a==9")))
+(define my-lexer (lex-this simple-math-lexer (open-input-string "return 5")))
 (let ((parser-res (simple-math-parser my-lexer))) parser-res)
 
